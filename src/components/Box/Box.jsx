13fe8e0b9175d935af38b7cd86cb7512415c2e4c
natikagrin <natikagrin@gmail.com>
2022-10-08 22:50:10 +0300

    не працює локалхост

---
+++ src/components/Box/Box.jsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+import { color, space, layout, border, shadow, typography } from 'styled-system';
+
+export const Box = styled.div`
+  ${color}
+  ${space}
+  ${layout}
+  ${border}
+  ${shadow}
+  ${typography}
+`;
